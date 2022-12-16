c1da28c801ac100aa435b8641f38d7ee0b0ccf99
Aleksandr Belous <ab21082001@yandex.ru>
2022-12-16 16:50:20 +0300

    Improved add_New_Block fn

--- block_operations.py
+++ block_operations.py
@@ -1,9 +1,16 @@
 #
+import hashlib
 
 from main import create_Block
 from main_chain import chain, root, nonce_requirements
 import file_operations
-import itertools
+import string
+import random
+
+
+def random_String( ):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for _ in range(6))
 
 
 def get_Block_Info(lineNumber, blockNumber):
@@ -16,23 +23,37 @@ def get_Block_Info(lineNumber, blockNumber):
 
 def add_New_Block( ):
     print('in add_new_block fn')
-    cur_level_idx = root[0] - 1
+    
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # add NONCE to the end of block
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
     alphabet = '0123456789'
     pr_hash = None
+    nonce = None
     while True:
-        nonce = None
-        for letter in itertools.product(alphabet, repeat = 6):
-            nonce = ''.join(letter)
+        nonce = random_String( )
         # make copy of block-file
-        pr_hash = file_operations.get_Block_Hash(root[0], root[1], nonce)
+        copy = get_Block_Info(root[0], root[1])
+        copy += nonce
+        # with open('tmp', 'w') as tmp:
+        #    tmp.write(copy)
+        # pr_hash = file_operations.get_Block_Hash(root[0], root[1], nonce)
+        pr_hash = hashlib.md5(copy.encode( )).hexdigest( )
         if pr_hash.count('0') == nonce_requirements:
             print(pr_hash)
             # create new block within this level
-            
+            last_str = ['nonce', nonce]
+            file_operations.save_new_Action_to_File(root[0], root[1], last_str)
             break
+    
+    # fixing the level
+    cur_level_idx = root[0] - 1
+    # fixing the new block number
+    cur_block_idx = root[1] - 1
+    # fixing the block
+    cur_block = chain[cur_level_idx][cur_block_idx]
+    cur_block.append({ 'nonce': nonce })
     
     # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # predict block's number: n=n+1
